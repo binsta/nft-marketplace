869dcb6e953202972f9a63fca4bc80972a928793
binsta <binstoncardoza@gmail.com>
2022-01-25 16:43:39 +0530

    tezt

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,34 +1,33 @@
 require("@nomiclabs/hardhat-waffle");
-require("dotenv").config;
 const fs = require("fs");
 const projectId = fs.readFileSync(".env").toString().trim() || "";
 const keyData = fs.readFileSync(".pkey", {
-  encoding: "utf8",
-  flag: "r",
+    encoding: "utf8",
+    flag: "r",
 });
 
 module.exports = {
-  defaultNetwork: "hardhat",
-  networks: {
-    hardhat: {
-      chainId: 31337, // config standard
-    },
-    ropsten: {
-      url: `https://ropsten.infura.io/v3/${projectId}`,
-    },
+    defaultNetwork: "hardhat",
+    networks: {
+        hardhat: {
+            chainId: 31337, // config standard
+        },
+        ropsten: {
+            url: `https://ropsten.infura.io/v3/${projectId}`,
+        },
 
-    mainnet: {
-      url: `https://mainnet.infura.io/v3/${projectId}`,
-      accounts: [keyData],
+        mainnet: {
+            url: `https://mainnet.infura.io/v3/${projectId}`,
+            accounts: [keyData],
+        },
     },
-  },
-  solidity: {
-    version: "0.8.4",
-    setting: {
-      optimizer: {
-        enabled: true,
-        runs: 200,
-      },
+    solidity: {
+        version: "0.8.4",
+        setting: {
+            optimizer: {
+                enabled: true,
+                runs: 200,
+            },
+        },
     },
-  },
 };
